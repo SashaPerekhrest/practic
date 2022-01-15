4ee5562ad23c9e238b3b460a800921af9f6ef464
JunkerSoft <chebupelmen20@gmail.com>
2022-01-15 14:13:26 +0500

    Initial commit

---
+++ ex19/Ex19/Ex19/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Ex19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arguments = Console.ReadLine().Split(" ");
+            var location = arguments[0];
+            var firstFile = arguments[1];
+            var secondFile = arguments[2];
+            
+            var fileToCopy = new FileInfo(location + firstFile);
+            fileToCopy.CopyTo(location + secondFile);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
