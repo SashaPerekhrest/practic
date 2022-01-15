f89a26fa9bb012cbe061193c066bb59587ff0f8b
JunkerSoft <chebupelmen20@gmail.com>
2022-01-15 23:48:39 +0500

    Ex27 Complited

---
+++ Ex27/Ex27/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace Ex27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = Directory.GetFiles("Books");
+            var fStr = new StreamWriter("otchet.txt", false);
+
+            foreach (var book in files)
+            {
+                try
+                { 
+                    var doc = new XmlDocument();
+                    doc.Load(book);
+
+                    var title = doc.GetElementsByTagName("title-info")[0];
+                    foreach (XmlNode tags in title)
+                    {
+                        if (tags.Name == "book-title")
+                            fStr.Write($"Название: {tags.InnerText} ");
+                        if (tags.Name == "author")
+                            fStr.Write($"Автор: {tags["first-name"]?.InnerText} {tags["last-name"]?.InnerText} ");
+                        if (tags.Name == "genre")
+                            fStr.Write($"Жанр: {tags.InnerText} ");
+                        if (tags.Name == "date")
+                            fStr.Write($"Дата: {tags.InnerText} ");
+                    }
+                }
+                catch (Exception e) { Console.WriteLine(e.Message); return; }
+                fStr.WriteLine();
+            }
+            fStr.Close();
+        }
+    }
+}
