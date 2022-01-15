26f41bd2f7995f6155769dcb925d30373194b265
JunkerSoft <chebupelmen20@gmail.com>
2022-01-15 15:30:12 +0500

    Ex20 complited

--- Ex20/Ex20/Program.cs
+++ Ex20/Ex20/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace Ex20
 {
@@ -7,6 +9,11 @@ namespace Ex20
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var fileToCopy = File.ReadAllText("fileToOpen.txt", Encoding.UTF8);
+            Console.WriteLine(fileToCopy);
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            File.WriteAllText("windows", fileToCopy, Encoding.GetEncoding(1251));
+            File.WriteAllText("dos", fileToCopy, Encoding.GetEncoding(866));
         }
     }
 }
---
+++ Ex20/readme.txt
@@ -0,0 +1,1 @@
+все тестовые файлы лежат в Ex20\Ex20\bin\Debug\netcoreapp3.1
