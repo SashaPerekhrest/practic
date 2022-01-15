29392bcc54424797d424f789df84786cfcf5dc1f
JunkerSoft <chebupelmen20@gmail.com>
2022-01-15 21:52:34 +0500

    Ex 34 complited

---
+++ Ex34/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex34
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = Cursor.Position.X + " " + Cursor.Position.Y;
+        }
+    }
+}
